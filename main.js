75116d4a745ba2c34a825dc567096df62a025cfc
DCstewieG <DCstewieG@gmail.com>
2014-09-05 17:45:32 -0500

    Separate test app from framework

---
+++ main.js
@@ -0,0 +1,31 @@
+﻿var bread = require('./free-bread');
+
+bread.init({
+  handlers: {
+    users: {
+      collection: 'users', // default
+      projections: { // optional
+        browse: {
+          name: 1
+        },
+        read: {
+          _id: 0
+        }
+      },
+      // singleProjection optional
+      model: 'Pricer.model.User',
+      restful: 'bread', // or true
+      key: '_id'
+    },
+    'claim-sets': {
+      restful: false,
+      handlers: {
+        preview: {
+          get: function previewClaimSet(req, res) {
+            res.end('Preview!');
+          }
+        }
+      }
+    }
+  }
+});
